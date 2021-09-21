de417bb4ea5606c6627141a8a2e8fb0b4ad56bbc
Tom Ritchford <tom@swirly.com>
2021-09-21 14:34:04 +0200

    Obsolete Python 3.5 and 3.5

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup
 
 _classifiers = [
     'Development Status :: 5 - Production/Stable',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
